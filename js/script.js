1961426d6b083a275ac227ae4ca0788e053a1e7e
Francesco-Matteucci <francescomatteucci@gmail.com>
2024-08-01 16:16:24 +0200

    added select tag in html and fix in js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ alert('Benvenuto, ti chiederemo dei dati per creare il tuo biglietto');
 
 //* Gestione dell'evento click sul bottone
 calculateButton.addEventListener('click', function (e) {
-    // Evito l'azione predefinita di un bottone type="submit"
     e.preventDefault();
 
     //? FASE RACCOLTA DATI
@@ -20,13 +19,13 @@ calculateButton.addEventListener('click', function (e) {
     console.log('Km-request', kmNeeded);
 
     //* 2 - Chiedere al passeggero l'età
-    const age = parseInt(document.getElementById('ageInput').value);
-    console.log('Age', age);
+    const ageCategory = document.getElementById('ageCategory').value;
+    console.log('Age Category', ageCategory);
 
     //? FASE DI ELABORAZIONE
     //! validation
-    if (isNaN(kmNeeded) || isNaN(age)) {
-        alert('Non ha inserito dei numeri sui campi richiesti');
+    if (isNaN(kmNeeded) || kmNeeded < 0) {
+        alert('Inserisci un numero valido di kilometri');
     } else {
         //* Calcolo del prezzo base del biglietto per km
         const baseTicket = kmNeeded * 0.21;
@@ -36,14 +35,20 @@ calculateButton.addEventListener('click', function (e) {
         let costumerTicket = baseTicket;
         let discountMessage = '';
 
-        //* se minorenne
-        if (age < 18) {
-            costumerTicket *= 0.8;
-            discountMessage = 'Congratulazioni! Hai appena ricevuto uno sconto del 20% per essere minorenne.';
-            //* se over 65
-        } else if (age >= 65) {
-            costumerTicket *= 0.6;
-            discountMessage = 'Congratulazioni! Hai appena ricevuto uno sconto del 40% per essere over 65.';
+        switch (ageCategory) {
+            case 'minorenne':
+                costumerTicket *= 0.8;
+                discountMessage = 'Congratulazioni! Hai appena ricevuto uno sconto del 20% per essere minorenne.';
+                break;
+            case 'over65':
+                costumerTicket *= 0.6;
+                discountMessage = 'Congratulazioni! Hai appena ricevuto uno sconto del 40% per essere over 65.';
+                break;
+            case 'maggiorenne':
+            default:
+                // Nessuno sconto per i maggiorenni
+                discountMessage = 'Grazie per averci scelto!';
+                break;
         }
 
         //* aggiungo due decimali al prezzo del biglietto, e il valore euro
@@ -52,7 +57,7 @@ calculateButton.addEventListener('click', function (e) {
 
         //? FASE DI OUTPUT
 
-        resultElement.innerHTML = `Grazie per averci scelto! Il prezzo del tuo biglietto è: <strong>${costumerTicket}</strong><br><br>
+        resultElement.innerHTML = `Il prezzo del tuo biglietto è: <strong>${costumerTicket}</strong><br><br>
             <i>${discountMessage}</i>`;
     }
 
