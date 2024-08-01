20a6f3ab59118f9907b6111686e5fa451d5322ad
Francesco-Matteucci <francescomatteucci@gmail.com>
2024-08-01 13:48:44 +0200

    added preparation phase

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//? FASE PREPARATORIA
+console.log('JS ok')
+//* Recupero elementi interessati dal DOM
+const kmsInputField = document.getElementById('kms');
+console.log('Kilometri Richiesti', kmsInputField);
+
+const ageField = document.getElementById('age');
+console.log('Età del Cliente', ageField);
+
+const resultElement = document.getElementById('result');
+console.log('Paragrafo per il Risultato', resultElement);
+
+const ticketButton = document.querySelector('button')
+console.log('Crea il tuo Ticket', ticketButton)
+
