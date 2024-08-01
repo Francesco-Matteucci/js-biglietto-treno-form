19a1f27d08c9b38405f2ae3b7c67375e2ebf00f0
Francesco-Matteucci <francescomatteucci@gmail.com>
2024-08-01 16:01:01 +0200

    added elaboration and output phases

--- js/script.js
+++ js/script.js
@@ -23,4 +23,37 @@ calculateButton.addEventListener('click', function (e) {
     const age = parseInt(document.getElementById('ageInput').value);
     console.log('Age', age);
 
+    //? FASE DI ELABORAZIONE
+    //! validation
+    if (isNaN(kmNeeded) || isNaN(age)) {
+        alert('Non ha inserito dei numeri sui campi richiesti');
+    } else {
+        //* Calcolo del prezzo base del biglietto per km
+        const baseTicket = kmNeeded * 0.21;
+        console.log('Base Ticket', baseTicket);
+
+        //* Calcolo percentuale di sconto se minorenne oppure over 65
+        let costumerTicket = baseTicket;
+        let discountMessage = '';
+
+        //* se minorenne
+        if (age < 18) {
+            costumerTicket *= 0.8;
+            discountMessage = 'Congratulazioni! Hai appena ricevuto uno sconto del 20% per essere minorenne.';
+            //* se over 65
+        } else if (age >= 65) {
+            costumerTicket *= 0.6;
+            discountMessage = 'Congratulazioni! Hai appena ricevuto uno sconto del 40% per essere over 65.';
+        }
+
+        //* aggiungo due decimali al prezzo del biglietto, e il valore euro
+        costumerTicket = costumerTicket.toFixed(2) + '€';
+        console.log('Ticket Price', costumerTicket);
+
+        //? FASE DI OUTPUT
+
+        resultElement.innerHTML = `Grazie per averci scelto! Il prezzo del tuo biglietto è: <strong>${costumerTicket}</strong><br><br>
+            <i>${discountMessage}</i>`;
+    }
+
 })
